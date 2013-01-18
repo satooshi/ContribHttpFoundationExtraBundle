73a1826c7133293324b41543c78eb931608330ef
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-01-18 12:24:35 +0900

    add dependency injection

---
+++ DependencyInjection/ContribHttpFoundationExtraExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Contrib\HttpFoundationExtraBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * ContribHttpFoundationExtraExtension.
+ *
+ * @author Kitamura Satoshi <with.no.parachute@gmail.com>
+ */
+class ContribHttpFoundationExtraExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('view.xml');
+    }
+}
