b23bb5bbf4feade98bcbf44eabf6cd21ea04b74b
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-01-18 13:02:15 +0900

    fix bug: remove unnecessary config

--- DependencyInjection/ContribHttpFoundationExtraExtension.php
+++ DependencyInjection/ContribHttpFoundationExtraExtension.php
@@ -16,9 +16,6 @@ class ContribHttpFoundationExtraExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = $this->getConfiguration($configs, $container);
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('view.xml');
     }
